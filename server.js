77ad744f1925fbfb16be15478996de0bac0fa139
Grygorii Nakonechnyi <nakan88@gmail.com>
2019-03-24 18:14:11 +0200

    finish first subtask with get and check address

--- server.js
+++ server.js
@@ -31,6 +31,30 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+/* 
+1) should return next result on post /api/shorturl/new - https://www.google.com
+2) save address to the db;
+3) on success return address id;
+4) on get address by this id return origin address;
+*/
+// short url task
+const dns = require('dns');
+
+app.get("/api/shorturl/:url", function (req, res) {
+  var urlParam = req.params.url;
+  var regex = /\b(?!new|\s-\s).+/;
+  var url = urlParam.match(regex)[0];
+  
+  dns.lookup(url, (err) => {
+    if (err) {
+      res.json({error: err});
+    }
+    var sendData = {"original_url":url,"short_url":1}
+    res.json(sendData);
+  });
+  
+});
+
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
